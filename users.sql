4d71c45a3001075b1df3fa3deeb01ca8dc675ae4
matthoff56 <mattstierhoff@gmail.com>
2014-03-10 18:58:48 +0000

    test

---
+++ users.sql
@@ -0,0 +1,22 @@
+DROP DATABASE userDB;
+
+CREATE DATABASE IF NOT EXISTS userDB;
+GRANT ALL PRIVILEGES ON userDB.* to 'user'@'localhost' 
+identified by 'password';
+USE userDB;
+
+CREATE TABLE users
+(
+  firstname VARCHAR(25),
+  lastname VARCHAR(25),
+  username VARCHAR(15),
+  password VARCHAR(15),
+  city VARCHAR(25),
+  state CHAR(2),
+  school VARCHAR(30),
+  interests VARCHAR(160),
+  games VARCHAR(100)
+);
+  
+INSERT INTO abductions VALUES ('2010-09-07', 'ann', 'russo', 'Cranford', 'NJ');
+INSERT INTO abductions VALUES ('2010-09-07', 'Paul', 'Jackman', 'Westfield', 'NJ');
