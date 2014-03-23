446b413c9c9c93edbf22ff79ae939ce720e31c3e
Jason <jnemeth@mail.umw.edu>
2014-03-23 19:19:45 +0000

    Put users in 3rd normal form, linked it with userInfo

--- lanDB.sql
+++ lanDB.sql
@@ -10,9 +10,7 @@ CREATE TABLE IF NOT EXISTS games(
   id INTEGER NOT NULL AUTO_INCREMENT,
   title VARCHAR(30),
   rating VARCHAR(1),
-  userid INTEGER,
-  PRIMARY KEY (id),
-  FOREIGN KEY (userid) REFERENCES user(id)
+  PRIMARY KEY (id)
 )ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4;
 
 --
@@ -30,16 +28,23 @@ INSERT INTO games (id, title, rating) VALUES
 
 CREATE TABLE IF NOT EXISTS users(
   id INTEGER NOT NULL AUTO_INCREMENT,
+  userid INTEGER,
   firstname VARCHAR(25),
-  lastname VARCHAR(25),  
+  lastname VARCHAR(25),
+  game INTEGER,
+  PRIMARY KEY (id),
+  FOREIGN KEY (game) REFERENCES game(id),
+  FOREIGN KEY (userid) REFERENCES userInfo(id)
+)ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS userInfo(
+  id INTEGER,
   school VARCHAR(30),
   city VARCHAR(25),
-  state VARCHAR(2),  
-  game VARCHAR(30),
-  gameid INTEGER,
-  PRIMARY KEY (id),
-  FOREIGN KEY (gameid) REFERENCES game(id)
-)ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+  state VARCHAR(2),
+  PRIMARY KEY (id)
+)ENGINE=MyISAM  DEFAULT CHARSET=latin1;;
+  
 
 --
 -- Dumping data for the user accounts table
