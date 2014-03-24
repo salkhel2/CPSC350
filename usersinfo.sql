6b7c7820d0768da3bb231a87348228ef57efdb15
salkhel2 <salkhel2@mail.umw.edu>
2014-03-24 03:05:01 +0000

    edits

--- usersinfo.sql
+++ usersinfo.sql
@@ -20,6 +20,23 @@ CREATE TABLE users
   games VARCHAR(100) default NULL,
   PRIMARY KEY (user_id)
 );
-  
+
 INSERT INTO  VALUES ( user_id, 'ann', 'russo', 'russoa', 'russoa@live.com', 'F', 'pass1234', 'Cranford', 'NJ', 'UNJ', 'games', 'call of duty');
 INSERT INTO  VALUES ( user_id, 'Paul', 'Jackman', 'biggamer', 'paul@g.com', 'M', 'abcd1234', 'Westfield', 'NJ',  'UNJ', 'love playing games', 'call of duty');
+
+CREATE DATABASE IF NOT EXISTS session;
+GRANT ALL PRIVILEGES ON session.* to 'assist'@'localhost' identified by 'assist';
+USE session;
+
+CREATE TABLE IF NOT EXISTS `login` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(12) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `zipcode` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+INSERT INTO `login` (`id`, `username`, `password`, `zipcode`) VALUES
+(1, 'sam', SHA2('p00d13',0), 22314),
+(2, 'mike', SHA2('changeme',0), 22401),
+(3, 'sallie', SHA2('qwerty',0), 20007);
