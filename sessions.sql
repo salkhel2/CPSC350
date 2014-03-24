0a7535ce7851df32e9c61b27ca8f14177dfe66fb
salkhel2 <salkhel2@mail.umw.edu>
2014-03-24 03:05:57 +0000

    commented out for a while

--- sessions.sql
+++ sessions.sql
@@ -1,3 +1,4 @@
+/*
 CREATE DATABASE IF NOT EXISTS session;
 GRANT ALL PRIVILEGES ON session.* to 'assist'@'localhost' identified by 'assist';
 USE session;
@@ -9,3 +10,4 @@ CREATE TABLE IF NOT EXISTS `users` (
   `zipcode` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+*/
